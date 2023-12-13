5697c4c7b3f7bf57ecd9383a215fbf26a6bc48e7
Pernilla-Strandberg <>
2023-12-13 11:43:57 +0000

    Feat: Create 2nd superuser for test purpose, add rest login/logout views

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Include login/logout view
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
 ]
